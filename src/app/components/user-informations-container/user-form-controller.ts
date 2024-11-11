b4d03d23fabc66a184fe519320401731acff3874
Pedro Lima <pedro.oficial1969@gmail.com>
2024-11-11 14:09:19 -0300

    adiciona formulario de usuario.

---
+++ src/app/components/user-informations-container/user-form-controller.ts
@@ -0,0 +1,119 @@
+import { inject } from "@angular/core";
+import { FormGroup, FormBuilder, Validators, FormArray } from "@angular/forms";
+import { IUser } from "../../interfaces/user/user.interface";
+import { PhoneList } from "../../types/phone-list.types";
+import { AddressList } from "../../types/address.list.type";
+import { DependentsList } from "../../types/dependents-list.type";
+
+export class UserFormController {
+  userForm!: FormGroup;
+
+  private _fb = inject(FormBuilder);
+
+  constructor() {
+    this.createUserForm();
+  };
+
+  get generalInformations(): FormGroup {
+    return this.userForm.get('generalInformations') as FormGroup;
+  };
+
+  get phoneList(): FormArray {
+    return this.userForm.get('contactInformations.phoneList') as FormArray;
+  };;
+
+  get addressList(): FormArray {
+    return this.userForm.get('contactInformations.addressList') as FormArray;
+  }
+
+  get dependentsList(): FormArray {
+    return this.userForm.get('dependentsList') as FormArray;
+  };
+
+  fulFillUserForm(user: IUser) {
+    this.resetUserForm();
+
+    this.fulFillGeneralInformations(user);
+    this.fulFillPhoneList(user.phoneList);
+    this.fulFillAddressList(user.addressList);
+    this.fulFillDependentsList(user.dependentsList);
+  };
+
+  private resetUserForm() {
+    this.userForm.reset();
+
+    this.generalInformations.reset();
+
+    this.phoneList.reset();
+    this.phoneList.clear();
+
+    this.addressList.reset();
+    this.addressList.clear();
+
+    this.dependentsList.reset();
+    this.dependentsList.clear();
+  };
+
+  private fulFillGeneralInformations(user: IUser) {
+    this.generalInformations.patchValue(user);
+  };
+
+  private fulFillPhoneList(userPhoneList: PhoneList) {
+    userPhoneList.forEach((phone) => {
+      this.phoneList.push(
+        this._fb.group({
+          type: [phone.type, Validators.required],
+          areaCode: [phone.areaCode, Validators.required],
+          internationalCode: [phone.internationalCode, Validators.required],
+          number: [phone.number, Validators.required],
+        })
+      );
+    });
+  };
+
+  private fulFillAddressList(userAddressList: AddressList) {
+    userAddressList.forEach((address) => {
+      this.addressList.push(
+        this._fb.group({
+          type: [address.type, Validators.required],
+          street: [address.street, Validators.required],
+          complement: [address.complement, Validators.required],
+          country: [address.country, Validators.required],
+          state: [address.state, Validators.required],
+          city: [address.city, Validators.required],
+        })
+      );
+    });
+  };
+
+  private fulFillDependentsList(userDependentsList: DependentsList) {
+    userDependentsList.forEach((dependent) => {
+      this.dependentsList.push(
+        this._fb.group({
+          name: [dependent.name, Validators.required],
+          age: [dependent.age, Validators.required],
+          document: [dependent.document, Validators.required],
+        })
+      );
+    })
+  };
+
+  private createUserForm() {
+    this.userForm = this._fb.group({
+      generalInformations: this._fb.group({
+        name: ['', Validators.required],
+        email: ['', Validators.required],
+        country: ['', Validators.required],
+        state: ['', Validators.required],
+        maritalStatus: [null, Validators.required],
+        monthlyIncome: [null, Validators.required],
+        birthDate: [null, Validators.required],
+      }),
+      contactInformations: this._fb.group({
+        phoneList: this._fb.array([]),
+        addressList: this._fb.array([]),
+      }),
+      dependentsList: this._fb.array([]),
+    });
+  };
+};
--- src/app/components/user-informations-container/user-informations-container.component.ts
+++ src/app/components/user-informations-container/user-informations-container.component.ts
@@ -1,18 +1,28 @@
 import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { IUser } from '../../interfaces/user/user.interface';
+import { UserFormController } from './user-form-controller';
 
 @Component({
   selector: 'app-user-informations-container',
   templateUrl: './user-informations-container.component.html',
   styleUrl: './user-informations-container.component.scss',
 })
-export class UserInformationsContainerComponent implements OnChanges {
+export class UserInformationsContainerComponent extends UserFormController implements OnChanges {
   currentTabIndex: number = 0;
 
   @Input({ required: true }) isInEditMode: boolean = false;
   @Input({ required: true }) userSelected: IUser = {} as IUser;
 
-  ngOnChanges(_: SimpleChanges): void {
+  ngOnChanges(changes: SimpleChanges): void {
     this.currentTabIndex = 0; // Quando houver mudanças no Input, o index do Tab, volta para o inicio (no caso volta para tab "Geral").
+
+    // Verificando se os dados do usuário selecionada esteja preenchido ou seja não esteja vazio.
+    const HAS_USER_SELECTED = changes['userSelected'] && Object.keys(changes['userSelected'].currentValue).length > 0;
+
+    if (HAS_USER_SELECTED) {
+      // Também a possibilidade de passar para o parâmetro do método 'fulFillUserForm' o valor 'changes['userSelected'].currentValue', daria o mesmo resultado final.
+      // Pois, o currentValue também é o valor do usuário selecionado.
+      this.fulFillUserForm(this.userSelected);
+    }
   }
 }
