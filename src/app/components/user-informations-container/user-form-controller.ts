c10da8ca4ed24d3309249542efea51f3cb8c4d22
Pedro Lima <pedro.oficial1969@gmail.com>
2024-11-12 18:25:49 -0300

    adiciona configuracoes do "date" da data de nascimento.

--- src/app/components/user-informations-container/user-form-controller.ts
+++ src/app/components/user-informations-container/user-form-controller.ts
@@ -4,6 +4,7 @@ import { IUser } from "../../interfaces/user/user.interface";
 import { PhoneList } from "../../types/phone-list.types";
 import { AddressList } from "../../types/address.list.type";
 import { DependentsList } from "../../types/dependents-list.type";
+import { convertPtBrDateToDateObj } from "../../utils/convert-pt-br-date-to-date-obj";
 
 export class UserFormController {
   userForm!: FormGroup;
@@ -56,7 +57,12 @@ export class UserFormController {
   }
 
   private fulFillGeneralInformations(user: IUser) {
-    this.generalInformations.patchValue(user);
+    const newUser = {
+      ...user,
+      birthDate: convertPtBrDateToDateObj(user.birthDate),
+    };
+
+    this.generalInformations.patchValue(newUser);
   }
 
   private fulFillPhoneList(userPhoneList: PhoneList) {
