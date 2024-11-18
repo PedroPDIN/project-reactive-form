e6dcbc297f25bf73afe8806dafc33583f3be37de
Pedro Lima <pedro.oficial1969@gmail.com>
2024-11-17 22:03:33 -0300

    adiciona logica de dasabilitar o usuario.

--- src/app/components/users-list/users-list.component.ts
+++ src/app/components/users-list/users-list.component.ts
@@ -10,10 +10,15 @@ export class UsersListComponent {
   userSelectedIndex: number | undefined;
 
   @Input({ required: true }) usersList!: UsersListResponse;
+  @Input({ required: true }) isInEditMode: boolean = false;
 
   @Output('onUserSelected') onUserSelectedEmit = new EventEmitter<number>();
 
   onUserSelected(index: number) {
+    // ser estiver em mode de edição ira retorna nada imediatamente, com isso, não irá prosseguir/ignorar com o código abaixo.
+    // em resumo, quando um usuário estiver selecionado e estiver em nodo de edição, não será possível selecionar outro usuário.
+    if (this.isInEditMode) return;
+
     this.userSelectedIndex = index;
     this.onUserSelectedEmit.emit(index);
   }
